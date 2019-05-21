983cd5663389343c313acc143c515c04ba6ac1a9
chuxu <234185407@qq.com>
2019-05-21 10:51:19 +0800

    add distance

--- geometry/src/main/groovy/com/meorient/geometry/GeoController.groovy
+++ geometry/src/main/groovy/com/meorient/geometry/GeoController.groovy
@@ -3,19 +3,27 @@ package com.meorient.geometry
 import org.bson.types.BasicBSONList
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
+import org.springframework.data.geo.Distance
+import org.springframework.data.geo.Point
+import org.springframework.data.mongodb.core.MongoOperations
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.aggregation.Aggregation
 import org.springframework.data.mongodb.core.aggregation.AggregationResults
+import org.springframework.data.mongodb.core.aggregation.GeoNearOperation
 import org.springframework.data.mongodb.core.query.BasicQuery
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.NearQuery
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.geo.Metrics
 
 import com.alibaba.fastjson.*
 import com.mongodb.BasicDBObject
 import com.mongodb.DBObject
+import com.mongodb.client.AggregateIterable
 
 @RestController
 class GeoController {
@@ -43,9 +51,9 @@ class GeoController {
 	@RequestMapping(value="/v1/customers/location/api",method = RequestMethod.GET)
 	String nearby(Double longitude,Double latitude,Integer distance,String keyword,Integer limit) {
 		// 默认值
-		longitude = longitude ?: 120.371243
-		latitude = latitude ?: 30.30317
-		distance = distance ?: 5000
+		longitude = longitude ?: 120.377559
+		latitude = latitude ?: 30.309529
+		distance = distance ?: 1000
 		limit = limit ?: 5
 		keyword = keyword ?: ''
 		// 通过h2实现地理位置查询
@@ -53,23 +61,12 @@ class GeoController {
 		//def list = mapper.find(1L)
 		
 		// 通过mongodb实现地理附近查询
-		def point = [longitude,latitude]
-		BasicDBObject obj = new BasicDBObject("location",new BasicDBObject('$nearSphere',new BasicDBObject('$geometry',
-				new BasicDBObject("type","Point").append('coordinates', point)).append('$maxDistance', distance)))
-		.append("name", new BasicDBObject('$regex' , keyword ).append('$options', 'i'))
-		Query query = new BasicQuery(String.valueOf(obj)).limit(limit)
-		def list = mongoTemplate.find(query, Customer,"geometry") // 根据距离查询，无具体距离
-		
-		//展开数组
-//		DBObject queryUnwind=new BasicDBObject('$geoNear',
-//			new BasicDBObject('near',new BasicDBObject('type','Point')
-//				.append('coordinates', point)).append('distanceField', 'distance').append('spherical', true))
-		//分组统计
-//		DBObject groupObject=new BasicDBObject('_id',new BasicDBObject('item_name', '$items.item_name'));
-//		groupObject.put('min', new BasicDBObject('$min','$items.score'));
-//		groupObject.put('max', new BasicDBObject('$max','$items.score'));
-//		groupObject.put('avg', new BasicDBObject('$avg','$items.score'));
-//		DBObject  queryGroup=new BasicDBObject('$group',groupObject);
+		def location = new Point(longitude , latitude);
+		def query = NearQuery.near(location).maxDistance(new Distance(distance, Metrics.MILES)).num(limit).spherical(true)
+		.distanceMultiplier(6378137).query(new Query(Criteria.where("name").regex(".*"+keyword+".*")))
+		def operation = new GeoNearOperation(query , "distance")
+		def aggregation = new Aggregation(operation)
+		def results = mongoTemplate.aggregate(aggregation,"geometry",Customer)
 		
 //		AggregationResults results = mongoTemplate.aggregate(queryUnwind, Customer,'geometry');
 		
@@ -89,7 +86,7 @@ class GeoController {
 		cust.distance = distance
 		cust.limit = limit
 		cust.keyword = keyword
-		cust.details = list
+		cust.details = results.getMappedResults()
 		return JSON.toJSONString(cust)
-	}
+	} 
 }
